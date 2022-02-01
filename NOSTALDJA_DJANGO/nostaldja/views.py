d0d45bc54154679b8e7bd7ad5b9026b7e2ee5cc0
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-01 15:04:50 -0800

    completed views, urls, and templates for decade and fad lists

--- NOSTALDJA_DJANGO/nostaldja/views.py
+++ NOSTALDJA_DJANGO/nostaldja/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Decade, Fad
 
 # Create your views here.
+
+def decade_list(req):
+  decades = Decade.objects.all()
+  return render(req, 'nostaldja/decade_list.html', {'decades': decades})
+
+def fad_list(req):
+  fads = Fad.objects.all()
+  return render(req, 'nostaldja/fad_list.html', {'fads': fads})
