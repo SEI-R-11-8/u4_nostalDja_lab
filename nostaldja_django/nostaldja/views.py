c9113318ec46f62c8ff76233714ff6838e3a8ee0
vladkim91 <vkcoding21@gmail.com>
2022-02-01 18:47:44 -0800

    fads and decades rendered

--- nostaldja_django/nostaldja/views.py
+++ nostaldja_django/nostaldja/views.py
@@ -1,3 +1,12 @@
+# tunr/views.py
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Decade, Fad
+
+def decade_list(request):
+    decades = Decade.objects.all()
+    return render(request, 'nostaldja/decade_list.html', {'decades': decades})
+
+def fad_list(request):
+    fads = Fad.objects.all()
+    return render(request, 'nostaldja/fad_list.html', {'fads': fads})
