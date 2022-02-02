76453292d8fa20c867c8b946a3144ccd902059a4
CaseyStewart1 <caseystewartdesigns@mail.com>
2022-02-02 08:52:25 -0500

    hw finished but still needs debugging

---
+++ nostaldja_django/nostaldja/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Decade
+from .models import Fad
+
+# Register your models here.
+# caseystewart casey@casey.com casey
+
+
+admin.site.register(Decade)
+admin.site.register(Fad)
