0daa6ff06ec8cd3dab177e815bdadd9ebfb680dc
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-02 06:55:37 -0800

    added .env and .gitignore for security

--- NOSTALDJA_DJANGO/nostaldja_django/settings.py
+++ NOSTALDJA_DJANGO/nostaldja_django/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5na^f4u+q4531fx)idr_5dfy5tx*oxq18tr%8@nbqav%3a&vlx'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
