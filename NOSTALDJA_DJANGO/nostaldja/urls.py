578aea3aa54d427bb5e862e149d4c81f0017bcbe
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-01 17:09:16 -0800

    added views, urls, and templates for decade and fad details

--- NOSTALDJA_DJANGO/nostaldja/urls.py
+++ NOSTALDJA_DJANGO/nostaldja/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.decade_list, name='decade_list'),
-    path('fads/', views.fad_list, name='fad_list')
+    path('fads/', views.fad_list, name='fad_list'),
+    path('decades/<int:pk>', views.decade_details, name='decade_details'),
+    path('fads/<int:pk>', views.fad_details, name='fad_details')
 ]
--- NOSTALDJA_DJANGO/nostaldja/views.py
+++ NOSTALDJA_DJANGO/nostaldja/views.py
@@ -10,3 +10,11 @@ def decade_list(req):
 def fad_list(req):
   fads = Fad.objects.all()
   return render(req, 'nostaldja/fad_list.html', {'fads': fads})
+
+def decade_details(req, pk):
+  decade = Decade.objects.get(id=pk)
+  return render(req, 'nostaldja/decade_details.html', {'decade': decade})
+
+def fad_details(req, pk):
+  fad = Fad.objects.get(id=pk)
+  return render(req, 'nostaldja/fad_details.html', {'fad': fad})
